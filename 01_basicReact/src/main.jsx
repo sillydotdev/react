fe703e08af139fd5a98c748775dd36a7b82c3356
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-02 19:04:04 +0530

    basic react

--- 01_basicReact/src/main.jsx
+++ 01_basicReact/src/main.jsx
@@ -30,14 +30,15 @@ function MyApp() {
 // const reactElement = React.createElement(
 //   'a',
 //   {href: "https://google.com", target: "_blank"},
-//   'click me to visit google'
+//   'click me to visit google',
 //   anotherUser 
 // )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <MyApp />
   </React.StrictMode>,
-  // reactElement
+  
 )
 
