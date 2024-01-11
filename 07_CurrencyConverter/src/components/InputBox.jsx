a5fb5f5a6d711555afd00f7ad1c71e6012f55ee7
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-11 23:02:09 +0530

    currencu converter using custom hooks and component reusability

--- 07_CurrencyConverter/src/components/InputBox.jsx
+++ 07_CurrencyConverter/src/components/InputBox.jsx
@@ -1,3 +1,5 @@
+import React, { useId } from "react";
+
 function InputBox({
   label,
   amount,
@@ -7,32 +9,42 @@ function InputBox({
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
-
   className = "",
 }) {
+  const amountInputId = useId();
   return (
-    <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">label</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
           disabled={amountDisable}
           value={amount}
-          onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select 
-        className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-        value={selectCurrency}
-        onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-        disabled={currencyDisable}
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
         >
           {currencyOptions.map((currency) => (
-            <option key={currency} value={currency}>{currency}</option>    // key is importatnt to write when u use loops like map in jsx
+            <option key={currency} value={currency}>
+              {currency}
+            </option> // key is importatnt to write when u use loops like map in jsx
           ))}
         </select>
       </div>
